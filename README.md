c67db84a608354850548825aa6f0bc6d0bd5dd14
Shin'ichiro SUZUKI <shin@szk-engineering.com>
2017-06-02 04:00:49 +0900

    add RTC to rx_leonard

--- README.md
+++ README.md
@@ -1,2 +1,34 @@
 evoltalt
 ====
+
+<!-- TOC depthFrom:2 depthTo:6 withLinks:1 updateOnSave:1 orderedList:0 -->
+
+- [TX](#tx)
+	- [Qduino mini](#qduino-mini)
+- [RX](#rx)
+	- [Arduino Leonard](#arduino-leonard)
+		- [IM315SHLDv2](#im315shldv2)
+		- [Adafruit Data Logger Shield rev B](#adafruit-data-logger-shield-rev-b)
+			- [Pin Assign](#pin-assign)
+
+<!-- /TOC -->
+
+## TX
+### Qduino mini
+
+## RX
+### Arduino Leonard
+#### IM315SHLDv2
+#### Adafruit Data Logger Shield rev B
+SD card works well with FAT32 format.
+RTC chip is PCF8523.
+Battery for RTC is CR1220.
+It needs [RTClib](https://github.com/adafruit/RTClib).
+
+##### Pin Assign
+* ICSP SCK - SPI clock
+* ICSP MISO - SPI MISO
+* ICSP MOSI - SPI MOSI
+* Digital #10 - SD Card chip select (can cut a trace to re-assign)
+* SDA not connected to A4
+* SCL not connected to A5
--- rx_leonard/rx_leonard.ino
+++ rx_leonard/rx_leonard.ino
@@ -1,28 +1,58 @@
+#include <Wire.h>
+#include <RTClib.h>
+#include <SD.h>
 #include <SoftwareSerial.h>
+
+RTC_PCF8523 rtc;
 
 SoftwareSerial IM920(8, 9);  // RX, TX
 int busy;  // IM920 BUSY status
 
 void setup() {
   pinMode(10, INPUT);  // IM920 BUSY pin
+
   Serial.begin(19200);  // to PC
+
+  if (! rtc.begin()) {
+    Serial.println("Couldn't find RTC.");
+    while (1);
+  }
+  if (! rtc.initialized()) {
+    Serial.println("RTC isn't running.");
+    rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
+  }
+
   IM920.begin(19200);  // to IM920
   delay(1000);
   IM920.println("ECIO");  // set Character Mode
 }
 
-char input[63];
+char recv[63];
 int idx = 0;
 
 void loop() {
+  DateTime now = rtc.now();
+
   do {
     busy = digitalRead(10);
   } while (busy != 0);
   if (IM920.available()) {
-    input[idx] = IM920.read();
-    if (63 < idx || input[idx] == '\n') {
-      input[idx] = '\n';
-      Serial.println(input);
+    recv[idx] = IM920.read();
+    if (63 < idx || recv[idx] == '\n') {
+      recv[idx] = '\n';
+      Serial.print(now.year(), DEC);
+      Serial.print('/');
+      Serial.print(now.month(), DEC);
+      Serial.print('/');
+      Serial.print(now.day(), DEC);
+      Serial.print(" ");
+      Serial.print(now.hour(), DEC);
+      Serial.print(':');
+      Serial.print(now.minute(), DEC);
+      Serial.print(':');
+      Serial.print(now.second(), DEC);
+      Serial.print(" -> ");
+      Serial.println(recv);
       idx = 0;
     } else {
       idx++;
@@ -30,4 +60,3 @@ void loop() {
   }
   // delay(50);
 }
-
