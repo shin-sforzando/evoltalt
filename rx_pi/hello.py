cead670b7cafa06708c052985257f7487ddb1580
Shin'ichiro SUZUKI <shin@szk-engineering.com>
2017-06-18 22:43:27 +0000

    add hello.py using ibmiotf

---
+++ rx_pi/hello.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import sys
+import time
+import pprint
+import uuid
+
+import ibmiotf.application
+import ibmiotf.device
+
+try:
+  options = ibmiotf.device.ParseConfigFile("./config.cfg")
+  client = ibmiotf.device.Client(options)
+except ibmiotf.ConnectionException as e:
+  print(str(e))
+  sys.exit()
+
+client.connect()
+
+client.publishEvent("hello", "json", "hello")
+
+client.disconnect()
+
