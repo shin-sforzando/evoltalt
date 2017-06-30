9dfdfda2728a343757abcde508ce1024b1406e30
Shin'ichiro SUZUKI <shin@szk-engineering.com>
2017-06-30 14:22:42 +0000

    add NDIGITS

--- rx_pi/main.py
+++ rx_pi/main.py
@@ -15,6 +15,11 @@ import dweepy
 import requests
 
 
+BASE_NDIGITS = 6
+CURRENT_NDIGITS = 4
+ALT_NDIGITS = 6
+ALT_MA_SIZE = 5
+
 os.environ["COLOREDLOGS_LOG_FORMAT"] = "[%(levelname)s]\t%(asctime)s - %(message)s"
 os.environ["COLOREDLOGS_DATE_FORMAT"] = "%H:%M:%S"
 
@@ -34,43 +39,65 @@ regex = r"(?P<node>\d{2}),(?P<sender>\w{4}),(?P<rssi>\w{2}):\s(?P<charge>\d+)%\s
 pattern = re.compile(regex)
 
 
-def estimate_altitude(base_pressure, current_pressure, current_temperature):
-    return (((current_pressure / base_pressure) ** (1.0 / 5.275) - 1.0) * (current_temperature + 273.15)) / 0.0065
+queue_alt_ma = []
 
+def estimate_altitude(base_pressure, current_pressure, current_temperature):
+    a = (((current_pressure / base_pressure) ** (1.0 / 5.275) - 1.0) * (current_temperature + 273.15)) / 0.0065
+    queue_alt_ma.append(a)
+    if ALT_MA_SIZE < len(queue_alt_ma):
+        queue_alt_ma.pop(0)
+    return sum(queue_alt_ma) / len(queue_alt_ma)
 
 def get_location():
-    r = requests.get("http://ipinfo.io/")
-    return r.json()["loc"].split(",")
+    try:
+        response = requests.get("http://ipinfo.io/", timeout=1.0)
+    except requests.exceptions.RequestException as re:
+        logger.error(de)
+        return (0, 0)
+    return response.json()["loc"].split(",")
 
 
-lat, lng = get_location()
-logger.info("Latitude: %s Longitude: %s", lat, lng)
+last_update = datetime.now()
+last_altitude = 0
+idx = 0
 
 
 with serial.Serial("/dev/ttyUSB0", 19200) as ser:
-    with requests.Session() as session:
-        while ser.isOpen():
+    time.sleep(1)
+    while ser.isOpen():
+        with requests.Session() as session:
             rx_msg = ser.readline()
             m = pattern.match(rx_msg.decode('utf-8'))
             if m:
                 now = datetime.now()
+                delta_t = now - last_update
+                logger.info("# %s (Δ: %s [s])", "{0:06d}".format(idx), delta_t.total_seconds())
+
+                if idx % 1000 == 0:
+                    lat, lng = get_location()
+                    logger.info("Lat / Lng: %s / %s", lat, lng)
 
                 base_temperature, base_pressure, base_humidity = bme280.readBME280All()
-                logger.info("Base    : %shPa %sC %s%%", base_pressure, base_temperature, base_humidity)
+                base_pressure = round(base_pressure, BASE_NDIGITS)
+                base_temperature = round(base_temperature, BASE_NDIGITS)
+                base_humidity = round(base_humidity, BASE_NDIGITS)
+                logger.info("Base     : %shPa %sC %s%%", base_pressure, base_temperature, base_humidity)
 
-                logger.info("Current : %s", m.group())
+                logger.info("Current  : %s", m.group())
                 current_sender = m.group("sender")
                 current_rssi = int(m.group("rssi"), 16)
                 current_charge = int(m.group("charge"))
-                current_pressure = float(m.group("pressure"))
-                current_temperature = float(m.group("temperature"))
-                current_humidity = float(m.group("humidity"))
-                current_altitude = estimate_altitude(base_pressure, current_pressure, current_temperature)
-                logger.info("Altitude: %s[m]", current_altitude)
+                current_pressure = round(float(m.group("pressure")), CURRENT_NDIGITS)
+                current_temperature = round(float(m.group("temperature")), CURRENT_NDIGITS)
+                current_humidity = round(float(m.group("humidity")), CURRENT_NDIGITS)
+                current_altitude = round(estimate_altitude(base_pressure, current_pressure, current_temperature), ALT_NDIGITS)
+                speed = round((current_altitude - last_altitude) / delta_t.total_seconds(), ALT_NDIGITS)
+                logger.info("Altitude : %s[m] (%s [m/s])", current_altitude, speed)
 
                 data = {"timestamp": now.isoformat(), "sender": current_sender, "rssi": current_rssi, "charge": current_charge,
                         "base_temperature": base_temperature, "base_pressure": base_pressure, "base_humidity": base_humidity,
-                        "current_temperature": current_temperature, "current_pressure": current_pressure, "current_humidity": current_humidity, "altitude": current_altitude,
+                        "current_temperature": current_temperature, "current_pressure": current_pressure, "current_humidity": current_humidity,
+                        "altitude": current_altitude, "speed": speed,
                         "latitude": lat, "longitude": lng}
 
                 try:
@@ -78,3 +105,7 @@ with serial.Serial("/dev/ttyUSB0", 19200) as ser:
                 except dweepy.api.DweepyError as de:
                     logger.error(de)
                     time.sleep(1)
+
+                last_update = now
+                last_altitude = current_altitude
+                idx += 1
